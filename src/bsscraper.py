ce5ef8a8dac05da8d407d3914068d6986229c7fc
Steve Newton <newton36@gmail.com>
2021-02-27 09:53:17 -0600

    cleanup ttest file

--- src/bsscraper.py
+++ src/bsscraper.py
@@ -1,13 +1,15 @@
 import sqlite3
 import pandas as pd
-import urllib
+import urllib.request
 
 
 def savant_call(season, team, place, csv=False, sep=';'):
     """
-    Breaks data into team and year for reasonable file sizes.
+    Breaks data into team, year, and place for small file sizes.
+    Args: int, str, str, bool, str
+    Returns: csv or dataframe
     """
-    # Generate the URL to search based on team and year.
+    # Generate the URL
     url = ("https://baseballsavant.mlb.com/statcast_search/csv?all=true&"
            "hfPT=&hfAB=&hfGT=R%7C&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&"
            f"hfPull=&hfC=&hfSea={season}%7C&hfSit=&player_type=pitcher&hfOuts=&"
@@ -17,8 +19,9 @@ def savant_call(season, team, place, csv=False, sep=';'):
            "min_results=0&group_by=name&sort_col=pitches&player_event_sort="
            "api_p_release_speed&sort_order=desc&min_pas=0&type=details&")
     
-    # Returns csv if csv is True or a Dataframe otherwise.
-    return urllib.request.urlretrieve(url, f'{team}_{season}.csv') if csv else pd.read_csv(url)
+    file_name = f'{team}_{season}_{place}.csv'
+    # Returns csv if csv is True
+    return urllib.request.urlretrieve(url, file_name) if csv else pd.read_csv(url)
 
 
 teams = ['LAA', 'HOU', 'OAK', 'TOR', 'ATL', 'MIL', 'STL','CHC', 'ARI', 
@@ -29,10 +32,10 @@ teams = ['LAA', 'HOU', 'OAK', 'TOR', 'ATL', 'MIL', 'STL','CHC', 'ARI',
 
 def make_database(db_name, seasons, teams=teams):
     """
-    Creates a database. Uses all teams unless othrwise specified.
-    All data is loaded into a single table named statcast.
+    Creates a database with all teams unless othrwise specified.
+    Args: str, list, list
+    Returns: None
     """
-    # Create and connect to the database.
     savant = sqlite3.connect(f'{db_name}.db')
     
     where = ['Home', 'Road']
@@ -43,10 +46,9 @@ def make_database(db_name, seasons, teams=teams):
                 pd.io.sql.to_sql(savant_call(season, team, place),
                                  name='statcast', con=savant, if_exists='append')
 
-    # Close connection
     savant.commit()
     savant.close()
 
     
 if __name__ == "__main__":
-    make_database('NYY_NYM_2020', [2020], ['NYY', 'NYM'])
+    savant_call(2020, 'NYY', 'Away', True)
---
+++ src/welch_ttests.py
@@ -0,0 +1,78 @@
+import scipy.stats as scs
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def welch_ttest(x, df1, id1, df2, id2, pitch, stat):
+    '''
+    Saves a plot of the samples distributions then returns a t-test result.
+    '''
+    stat1 = df1[df1.pitch_type==pitch][stat]
+    stat2 = df2[df2.pitch_type==pitch][stat]
+
+    fig, ax = plt.subplots(figsize=(7,5))
+    ax.hist(stat1, alpha=.6, bins=x, label=id1)
+    ax.hist(stat2, alpha=.6, bins=x, label=id2)
+    ax.set_xlabel(stat)
+    ax.set_title(pitch)
+    ax.legend()
+
+    plt.savefig(f'visuals/ttests/ttest_{id1}_{id2}_{pitch}_{stat}.png')
+    print(f'A histogram plot named ttest_{id1}_{id2}_{pitch}_{stat}.png \n'
+            'was saved in ~/visuals/ttests. \n')
+
+    result = scs.ttest_ind(stat1, stat2, equal_var=False)
+
+    return (result[0], result[1], sum(df1.pitch_type==pitch), sum(df2.pitch_type==pitch))
+
+
+def print_test_results(df, id1, id2, pitch, stat, test_stats):
+    '''
+    Prints the results of the t-test with means.
+    '''
+    x = np.linspace(df[stat].min(), df[stat].max(), 50)
+    player1 = df[df.pitcher==id1]
+    player2 = df[df.pitcher==id2]
+    player1_means = player1.groupby('pitch_type').agg('mean')[test_stats]
+    player2_means = player2.groupby('pitch_type').agg('mean')[test_stats]
+
+    result = welch_ttest(x, player1, id1, player2, id2, pitch, stat)
+    
+    print(f'Welch\'s t-test result -{pitch} -{stat}:')
+    print('docs.scipy.org/doc/scipy/reference/generated/scipy.stats.ttest_ind.html')
+    print(f'ID{id1} mean: {round(player1_means[stat][pitch],5)}')
+    print(f'ID{id2} mean: {round(player2_means[stat][pitch],5)}')
+    print(f't-statistic: {round(result[0],5)}')
+    print(f'p-value: {result[1]}')
+    print(f'ID{id1} sample size: {result[2]}')
+    print(f'ID{id2} sample size: {result[3]}')
+
+
+def run_tests(df, id1, id2, test_pitches, test_stats):
+    '''
+    Prints a Welch's t-test result for specified stats, players, and pitches.
+    '''
+    for stat in test_stats:
+        for pitch in test_pitches:
+            print_test_results(df, id1, id2, pitch, stat, test_stats)
+
+
+if __name__ == "__main__":
+
+    '''To make this more useful, have a database of all statcast data then use the input
+    id to query the database and put player data into a dataframe, maybe even automate tests to look for interesting results.'''
+
+    #Load data
+    df = pd.read_csv('aces_2020.csv')
+    cole_id = 543037   #ID can be found in URL on Baseball Savant player page.
+    degrom_id = 594798
+
+    #Print Welch's t-test results
+    print('A Welch\'s t-test comparison.')
+    id1, id2 = input("Enter two player IDs: ").split(',')
+    pitches = input("Enter pitch types (FF, CU, SL, CH, FT, FS, etc.): ")
+    stats = input("Enter stat fields (release_speed, pfx_z, etc.): ")
+    print('\n')
+
+    run_tests(df, int(id1), int(id2), pitches.upper().split(','), stats.split(','))
